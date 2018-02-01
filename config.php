b3c0bec3a3ddd9f71dd6fcc5ff6afd5e10b15eb3
00Blabs <imnotevenflaws@gmail.com>
2018-02-01 10:59:57 +0100

    loadingscreen

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+
+//General
+$APIKey = "A10B552BF71C0A1F5E0CE3DD1CF100B9"; // Go get your API here: http://steamcommunity.com/dev/apikey
+
+$load_name ="FlawRP"; // For your Browser Name
+$community_name ="FlawRP"; // Your community/server name. The one in the middle
+$community_url = "www.flawrp.com"; // Url for your users to know where to go
+$intro = TRUE; // If you want to turn this off, type FALSE instead
+// Introduction
+$tagline = " A long time ago, in a server far,<br> far away...."; // The intro text in the beginning of the animation
+// Above Server Name
+$sub_title_1 = "Welcome to our community"; // Above your server name
+// Under Server Name
+$sub_title_2 = "read the rules to learn the ways of the force"; // Below your server name
+
+//Bottom Part
+$left_bottom_title = "About Us";
+$left_bottom_sub_title = "$community_url for more info";
+$right_bottom_title = "Our Server";
+$right_bottom_sub_title = "General information about the server";
+
+$left_bottom_text = "Not Set.";
+
+?>
